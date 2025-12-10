ed982a15f62ef54758aba3a9c28f11b38d7c12cc
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-12-10 13:15:53 +0900

    Changes to be committed: 컴파일 테스트 성공 	modified:   NeuroMap/cpp/__init__.py 	modified:   NeuroMap/cpp/bind.cpp 	modified:   test/test.ipynb

--- NeuroMap/cpp/__init__.py
+++ NeuroMap/cpp/__init__.py
@@ -1,12 +1,18 @@
 from torch.utils.cpp_extension import load
 import os
+import torch
+
+# 중요!!!
+torch_lib_path = os.path.join(os.path.dirname(torch.__file__), "lib")
+os.add_dll_directory(torch_lib_path)
 
 DIR = os.path.dirname(os.path.abspath(__file__))
-# https://github.com/pytorch/vision/issues/2360#issuecomment-883574291
+
 neuromap_cpp = load(
     name="neuromap_cpp",
     sources=[
-        os.path.join(DIR, "bind.cpp")
+        os.path.join(DIR, "bind.cpp"),
+        # os.path.join(DIR, "test.cpp"),
     ],
     verbose=True,
 )
--- NeuroMap/cpp/bind.cpp
+++ NeuroMap/cpp/bind.cpp
@@ -1,6 +1,7 @@
+#include <pybind11/pybind11.h>
 #include <torch/extension.h>
 #include "test.cpp"
 
-TORCH_LIBRARY(neuromap_cpp, m) {
-    m.def("test", &test);
+PYBIND11_MODULE(neuromap_cpp, m) {
+    m.def("test", &test, "Test function that multiplies tensor by 2");
 }
